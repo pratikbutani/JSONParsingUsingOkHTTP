fc9a2512a3e49865a8c64ca94ac6b392e6a54ae9
unknown <pratik13butani@gmail.com>
2015-12-13 16:50:05 +0530

    First Commit

---
+++ app/src/main/java/com/androidbuts/jsonparsing/MainActivity.java
@@ -0,0 +1,184 @@
+package com.androidbuts.jsonparsing;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.androidbuts.jsonparsing.parser.JSONParser;
+import com.androidbuts.jsonparsing.utils.InternetConnection;
+import com.androidbuts.jsonparsing.utils.Keys;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /**
+         * Array List for Binding Data from JSON to this List
+         */
+        list = new ArrayList<>();
+        /**
+         * Binding that List to Adapter
+         */
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
+
+        /**
+         * Getting List and Setting List Adapter
+         */
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Snackbar.make(findViewById(R.id.parentLayout), list.get(position), Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(@NonNull View view) {
+
+                /**
+                 * Just to know onClick and Printing Hello Toast in Center.
+                 */
+                Toast toast = Toast.makeText(getApplicationContext(), "Hello in Center", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
+
+                /**
+                 * Checking Internet Connection
+                 */
+                if (InternetConnection.checkConnection(getApplicationContext())) {
+                    new GetDataTask().execute();
+                } else {
+                    Snackbar.make(view, "Internet Connection Not Available", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Creating Get Data Task for Getting Data From Web
+     */
+    class GetDataTask extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /**
+             * Progress Dialog for User Interaction
+             */
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setTitle("Hey Wait Please...");
+            dialog.setMessage("I am getting your JSON");
+            dialog.show();
+        }
+
+        @Nullable
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            /**
+             * Getting JSON Object from Web Using okHttp
+             */
+            JSONObject jsonObject = JSONParser.getDataFromWeb();
+
+            try {
+                /**
+                 * Check Whether Its NULL???
+                 */
+                if (jsonObject != null) {
+                    /**
+                     * Check Length...
+                     */
+                    if(jsonObject.length() > 0) {
+                        /**
+                         * Getting Array named "contacts" From MAIN Json Object
+                         */
+                        JSONArray array = jsonObject.getJSONArray(Keys.KEY_CONTACTS);
+
+                        /**
+                         * Check Length of Array...
+                         */
+                        int lenArray = array.length();
+                        if(lenArray > 0) {
+                            for(int jIndex = 0; jIndex < lenArray; jIndex++) {
+                                /**
+                                 * Getting Inner Object from contacts array...
+                                 * and
+                                 * From that We will get Name of that Contact
+                                 *
+                                 */
+                                JSONObject innerObject = array.getJSONObject(jIndex);
+                                String name = innerObject.getString(Keys.KEY_NAME);
+
+                                /**
+                                 * Getting Object from Object "phone"
+                                 */
+                                JSONObject phoneObject = innerObject.getJSONObject(Keys.KEY_PHONE);
+                                String phone = phoneObject.getString(Keys.KEY_MOBILE);
+
+                                /**
+                                 * Adding name and phone concatenation in List...
+                                 */
+                                list.add(name + " - " + phone);
+                            }
+                        }
+                    }
+                } else {
+
+                }
+            } catch (JSONException je) {
+                Log.i(JSONParser.TAG, "" + je.getLocalizedMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            dialog.dismiss();
+            /**
+             * Checking if List size if more than zero then
+             * Update ListView
+             */
+            if(list.size() > 0) {
+                adapter.notifyDataSetChanged();
+            } else {
+                Snackbar.make(findViewById(R.id.parentLayout), "No Data Found", Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/androidbuts/jsonparsing/parser/JSONParser.java
@@ -0,0 +1,90 @@
+package com.androidbuts.jsonparsing.parser;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
+/**
+ * @author Pratik Butani
+ */
+public class JSONParser {
+
+    /********
+     * URLS
+     *******/
+    private static final String MAIN_URL = "http://pratikbutani.x10.mx/json_data.json";
+
+    /**
+     * TAGs Defined Here...
+     */
+    public static final String TAG = "TAG";
+
+    /**
+     * Key to Send
+     */
+    private static final String KEY_USER_ID = "user_id";
+
+    /**
+     * Response
+     */
+    private static Response response;
+
+    /**
+     * Get Table Booking Charge
+     *
+     * @return JSON Object
+     */
+    public static JSONObject getDataFromWeb() {
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(MAIN_URL)
+                    .build();
+            response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+        } catch (@NonNull IOException | JSONException e) {
+            Log.e(TAG, "" + e.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Get Particular Record
+     *
+     * @param userId UserId
+     * @return JSONObject
+     */
+    public static JSONObject getDataById(int userId) {
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody formBody = new FormEncodingBuilder()
+                    .add(KEY_USER_ID, Integer.toString(userId))
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(MAIN_URL)
+                    .post(formBody)
+                    .build();
+
+            response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+
+        } catch (IOException | JSONException e) {
+            Log.e(TAG, "" + e.getLocalizedMessage());
+        }
+        return null;
+    }
+}
